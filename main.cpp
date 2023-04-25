66696e4aa245c0cb4d1e5766369ab68567a1405d
admin <admin@BCS-Ubuntu-20.lrccd.ad.losrios.edu>
2023-04-25 15:12:46 -0700

    First commit mandelbrot

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include "ComplexPlane.h"
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <iostream>
+#include <sstream>
+using namespace sf;
+using namespace std;
+
+int main()
+{
+    //Constructs and constrains window
+    float width = sf::VideoMode::getDesktopMode().width;
+	float height = sf::VideoMode::getDesktopMode().height;
+	View mainView(FloatRect(0.0f,0.0f,width, height));
+	//width = 800; height = 600;
+	// Create a video mode object
+	VideoMode vm(width, height);
+	// Create and open a window for the game
+	RenderWindow window(vm, "Mandelbrot Set!", Style::Default);
+
+    //ComplexPlane comPlane;
+}
