1870cd7847c0d0766e5ad0e6c8521d757e936dfc
admin <admin@BCS-Ubuntu-12.lrccd.ad.losrios.edu>
2023-04-27 02:04:50 -0700

    Commit fom ubuntu Apr27

--- main.cpp
+++ main.cpp
@@ -18,8 +18,75 @@ int main()
 	// Create and open a window for the game
 	RenderWindow window(vm, "Mandelbrot Set!", Style::Default);
 
-    ComplexPlane comPlane(height/width);
+    ComplexPlane screen(height/width);
 	Font font;
 	Text basicText;
 	
+	VertexArray vArray;
+	vArray.setPrimitiveType(Points);
+	vArray.resize(width*height);
+	enum class GameState { CALCULATING, DISPLAYING };
+	GameState curr = GameState::CALCULATING;
+
+	while(window.isOpen())
+	{
+		/*HANDLE INPUT*/
+		Event event;
+		while(window.pollEvent(event))
+		{
+			if(event.type == Event::Closed)
+			{
+				window.close();
+			}
+			if(event.type == sf::Event::MouseButtonPressed)
+			{
+				// get the current mouse position in the window
+                sf::Vector2i pixelPos = sf::Mouse::getPosition(window);
+
+                // convert it to world coordinates
+                sf::Vector2f worldPos = window.mapPixelToCoords(pixelPos, screen);
+                if(event.mouseButton.button == sf::Mouse::Right)
+				{
+					screen.zoomOut();
+					screen.setCenter(worldPos);
+				}
+				if(event.mouseButton.button == sf::Mouse::Left)
+				{
+					screen.zoomIn();
+					screen.setCenter(worldPos);
+				}
+				curr = GameState::CALCULATING;                           
+			}
+			if(event.type == sf::Event::MouseMoved)
+			{
+				// get the current mouse position in the window
+                sf::Vector2i pixelPos = sf::Mouse::getPosition(window);
+
+                // convert it to world coordinates
+                sf::Vector2f worldPos = window.mapPixelToCoords(pixelPos, screen);
+                screen.setMouseLocation(worldPos);
+			}
+		}
+		if(Keyboard::isKeyPressed(Keyboard::Escape))
+		{
+			window.close();
+		}
+		/*UPDATE SCENE*/
+		if(curr == GameState::CALCULATING)
+		{
+			for(int j = 0; j < screen.getSize().x; j++)
+			{
+				for(int i = 0; i < screen.getSize().y; i++)
+				{
+					vArray[j + i * width].position = { (float)j,(float)i };
+					// get the current mouse position in the window
+                	sf::Vector2i pixelPos = { j, i };
+
+                	// convert it to world coordinates
+                	sf::Vector2f worldPos = window.mapPixelToCoords(pixelPos, screen);
+					
+				}
+			}
+		}
+	}
 }
